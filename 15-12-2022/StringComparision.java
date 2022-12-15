9ab0b257b870ad381fc0de4ea2083f5c48ad07d6
YogitaRokade <117341402+YogitaRokade@users.noreply.github.com>
2022-12-15 15:10:19 +0530

    Add files via upload

---
+++ 15-12-2022/StringComparision.java
@@ -0,0 +1,19 @@
+package arrayexamples;
+
+public class StringComparision {
+	public static void main(String []args)
+	{
+		String s1="hello";
+		String s2="hello";
+		String s3="good";
+		String s4="morning";
+		String s5="flag";
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.compareTo(s4));
+		System.out.println(s1.compareTo(s5));
+	
+	    
+	}
+
+}
