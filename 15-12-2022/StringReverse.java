0f4c29917a55ed70747348928a7c5b9c0fbbbab9
YogitaRokade <117341402+YogitaRokade@users.noreply.github.com>
2022-12-15 15:16:52 +0530

    Add files via upload

---
+++ 15-12-2022/StringReverse.java
@@ -0,0 +1,17 @@
+/*
+ * 3.write a program to print reverse of string
+ */
+
+package arrayexamples;
+
+import java.util.Scanner;
+
+public class StringReverse {
+	public static void main(String []args)
+	{
+		
+		StringBuffer buffer=new StringBuffer("Hello world");
+		
+		System.out.println(buffer.reverse());
+	}
+}
