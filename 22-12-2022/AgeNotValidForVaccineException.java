cb5a08fd2cfba6c8512bac517f4c33a8b2bb8314
YogitaRokade <117341402+YogitaRokade@users.noreply.github.com>
2022-12-23 16:15:18 +0530

    Add files via upload

---
+++ 22-12-2022/AgeNotValidForVaccineException.java
@@ -0,0 +1,11 @@
+package ExceptionHandeling;
+
+public class AgeNotValidForVaccineException extends Exception {
+
+	public AgeNotValidForVaccineException(String message)
+	{
+		super(message);
+	}
+	}
+
+
