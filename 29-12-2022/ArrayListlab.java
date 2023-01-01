c9552b92a0d15a621ab06e2b88682089b2e23183
YogitaRokade <117341402+YogitaRokade@users.noreply.github.com>
2023-01-01 23:09:49 +0530

    Add files via upload

---
+++ 29-12-2022/ArrayListlab.java
@@ -0,0 +1,26 @@
+/*
+ * 1.create Arraylist of type class Product.Perform operation:
+Product Class:
+states:productId,productName,productPrice,productStock
+behaviours:setter getter,constructors,toString()
+
+operations on Arraylist:
+create class ProductService and add these methods:
+1.create method addProduct() to add product details on arraylist.
+2.create getAllProduct() method to retrieve all details
+3.create deleteProductById() method to delete product by id.
+4.create deleteAllProduct() methods to delete all from list.
+5.create updateProductById() method to update product details.
+
+at end,convert the arraylist to Array.
+ */
+
+package thursdaylab;
+
+public class ArrayListlab {
+	public static void main(String args[])
+	{
+		
+	}
+
+}
